162826eabe788c27d28fa4d7ec09bb8438f4a3e0
TarikEren <1210606015@nku.edu.tr>
2024-05-26 17:22:20 +0300

    Created categoryController.js

---
+++ backend/controllers/categoryController.js
@@ -0,0 +1,34 @@
+const router = require("express").Router();
+
+router.route("/category")
+    //Get all categories
+    .get(() => {
+        try {
+
+        }
+        catch (error) {
+
+        }
+    })
+    //Create a category
+    .post(() => {
+        try {
+
+        }
+        catch (error) {
+
+        }
+
+    })
+
+router.route("/category/:name")
+    //Get one category
+    .get(() => {
+        try {
+
+        }
+        catch (error) {
+
+        }
+
+    })
