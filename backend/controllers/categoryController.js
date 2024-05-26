20f1c49cbda8f3f7f81aa514a5579e55faccbebf
TarikEren <1210606015@nku.edu.tr>
2024-05-26 21:43:22 +0300

    Fleshed out categoryController.js

--- backend/controllers/categoryController.js
+++ backend/controllers/categoryController.js
@@ -1,48 +1,69 @@
 const router = require("express").Router();
+const Category = require("../models/categoryModel");
 
-router.route("/category")
+router.route("/")
     //Get all categories
-    .get(() => {
+    .get(async (req, res) => {
         try {
-
+            const categories = await Category.find();
+            if (!categories) res.status(204).send({ message: "No categories found" });
         }
         catch (error) {
-
+            res.status(500).send({ message: error.message });
         }
     })
     //Create a category
-    .post(() => {
+    .post(async (req, res) => {
+        var category = new Category({
+            name: req.body.name
+        })
         try {
-
+            category = await Category.save();
+            res.status(201).send({ message: `Category with ID ${category._id} created successfully` });
         }
         catch (error) {
-
+            res.status(500).send({ message: error.message });
         }
     });
 
-router.route("/category/:name")
+router.route("/:id")
     //Get one category
-    .get(() => {
+    .get(async (req, res) => {
         try {
-
+            const category = await Category.findById(req.params.id);
+            if (!category) res.status(404).json({ message: "No such category found" });
         }
         catch (error) {
-
+            res.status(500).send({ message: error.message });
         }
     })
-    .put(() => {
+    .put(async (req, res) => {
         try {
-
+            const category = await Category.findByIdAndUpdate(
+                req.params.id,
+                {
+                    name: req.body.name || category.name
+                },
+                // { new: true }
+            )
+            res.status(200).send({message: `Edited category with ID ${category._id}`});
         }
         catch (error) {
-
+            res.status(500).send({message: error.message});
         }
     })
-    .delete(() => {
+    .delete(async (req, res) => {
         try {
-
+            Category.findByIdAndRemove(req.params.id).then(category => {
+                if (category) {
+                    return res.status(200).send({message: `Deleted category with ID ${category._id}`});
+                }
+                else {
+                    return res.status(404).send({message: `Category not found`});
+                }
+            })
         }
         catch (error) {
-
+            return res.status(500).send({message: error.message});
         }
     });
