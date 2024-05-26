7156bea5d3be16a167a3e9a1aeda573ad3ac4244
TarikEren <1210606015@nku.edu.tr>
2024-05-26 13:42:20 +0300

    Added userController.js

---
+++ backend/controllers/userController.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+
+const createToken = (_id) => {
+    return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "2h" });
+}
+
+router.route("/login")
+    .post(async (req, res) => {
+        const { email, password } = req.body;
+        try {
+            const user = await User.login(email, password);
+            const token = createToken(user._id);
+            res.status(200).json({ email, token });
+        } catch (err) {
+            res.status(400).json({ error: err.message });
+        }
+    });
+
+router.post("/register")
+    .post(async (req, res) => {
+        const { email, password } = req.body;
+        try {
+            const user = await User.signup(email, password);
+            const token = createToken(user._id);
+            res.status(200).json({ email, token });
+        } catch (err) {
+            res.status(400).json({ error: err.message });
+        }
+    });
+
+router.route("/cart")
+    .get(async (req, res) => {
+        //Show all products
+        try {
+
+        }
+        catch (err) {
+
+        }
+    })
+    .post(async (req, res) => {
+        //Add new products
+        let product;
+        try {
+
+        }
+        catch (err) {
+
+        }
+    })
+    .delete(async (req, res) => {
+        //Remove products
+        try {
+    
+        }
+        catch (err) {
+    
+        }
+    })
+
+
+router.get("/logout", () => {
+
+});
+
+module.exports = router;
