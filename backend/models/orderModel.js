956390b6009d3d502d6811cf3bcaee4d4ecc133c
TarikEren <1210606015@nku.edu.tr>
2024-05-27 20:12:27 +0300

    Restructure

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -1,100 +1,85 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-const orderSchema = new mongoose.Schema({
-    //ID of customer who ordered
-    customerId: {
-        type: String,
-        required: true
-    },
-
-    //Payment gateway (i.e. Stripe)
-    gateway: String,
-
-    //Type of payment (Credit, EFT etc.)
-    type: String,
-
-    //Credit card info if the user opts to pay using a credit card
-    card: {
-        brand: String,
-        lastFour: Number,
-        exprMonth: Number,
-        exprYear: Number,
-        cvvVerified: Boolean
-    },
-
-    //When the order was issued.
-    dateOrdered: {
-        type: Date,
-        default: Date.now()
-    },
-
-    //Price of the order
-    totalPrice: {
-        currency: String,
-        type: mongoose.Schema.Types.Decimal128,
-        default: 0
-    },
-
-    //Status of the order
-    status: {
-        type: String,
-        default: "Pending"
-    },
-
-    //Items that are in the order
+const orderSchema = mongoose.Schema({
     orderItems: [{
         type: mongoose.Schema.Types.ObjectId,
-        ref: "OrderItem",
-        required: true
+        ref: 'OrderItem',
+        required:true
     }],
-
-    //Destination address
     shippingAddress1: {
         type: String,
-        required: true
+        required: true,
     },
     shippingAddress2: {
         type: String,
-        default: ""
     },
-
     city: {
         type: String,
-        required: true
+        required: true,
     },
-
     zip: {
-        type: Number,
-        required: true
+        type: String,
+        required: true,
     },
-
+    country: {
+        type: String,
+        required: true,
+    },
     phone: {
-        type: Number,
-        required: true
+        type: String,
+        required: true,
     },
     status: {
         type: String,
-        default: "Pending"
+        required: true,
+        default: 'Pending',
+    },
+    totalPrice: {
+        type: Number,
     },
     user: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
     },
-
-    //Origin address
-    origin: {
-        type: String,
-        default: "Add origin address to the orderModel.js",
+    dateOrdered: {
+        type: Date,
+        default: Date.now,
     },
+})
 
-    //Carrier firm
-    carrier: {
-        type: String,
-        default: ""
-    },
+orderSchema.virtual('id').get(function () {
+    return this._id.toHexString();
+});
 
-    //Tracking number issued by the carrier
-    trackingNumber: Number
-}, { collection: "orders" });
+orderSchema.set('toJSON', {
+    virtuals: true,
+});
 
-module.exports = mongoose.model("Order", orderSchema);
+exports.Order = mongoose.model('Order', orderSchema);
+
+
+
+/**
+Order Example:
+
+{
+    "orderItems" : [
+        {
+            "quantity": 3,
+            "product" : "5fcfc406ae79b0a6a90d2585"
+        },
+        {
+            "quantity": 2,
+            "product" : "5fd293c7d3abe7295b1403c4"
+        }
+    ],
+    "shippingAddress1" : "Flowers Street , 45",
+    "shippingAddress2" : "1-B",
+    "city": "Prague",
+    "zip": "00000",
+    "country": "Czech Republic",
+    "phone": "+420702241333",
+    "user": "5fd51bc7e39ba856244a3b44"
+}
+
+ */
