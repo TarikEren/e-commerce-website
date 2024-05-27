d6fd4e55d0955741aeebfaac5c9690360ed124fc
TarikEren <1210606015@nku.edu.tr>
2024-05-27 20:10:35 +0300

    Bug fixes and comments

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
-const Order = require("../models/orderModel");
+const { Order } = require("../models/orderModel");
 const OrderItem = require("../models/orderItemModel");
 
 router.route("/")
+    //TODO: Needs admin auth
     .get(async (req, res) => {
         try {
             const orders = await Order.find().populate("user", "name").sort({ "dateOrdered": -1 });
@@ -16,7 +17,9 @@ router.route("/")
     })
     .post(async (req, res) => {
         try {
-            const orderItemsIds = Promise.all(req.body.order.map(async (orderItem) => {
+            const orderedItems = req.body.orderItems
+            if (!orderedItems) return res.status(400).send({ message: "No items provided" });
+            const orderItemsIds = Promise.all(orderedItems.map(async (orderItem) => {
                 let newOrderItem = new OrderItem({
                     quantity: orderItem.quantity,
                     product: orderItem.product
@@ -32,7 +35,7 @@ router.route("/")
             }))
             const totalPrice = totalPrices.reduce((a, b) => a + b, 0);
 
-            let order = newOrder({
+            let order = new Order({
                 orderItems: orderItemsIdsResolved,
                 shippingAddress1: req.body.shippingAddress1,
                 shippingAddress2: req.body.shippingAddress2,
@@ -42,18 +45,20 @@ router.route("/")
                 phone: req.body.phone,
                 status: req.body.status,
                 totalPrice: totalPrice,
+                currency: req.body.currency,
                 user: req.body.user,
                 card: req.body.card,
                 gateway: req.body.gateway || "Stripe", //Change later
                 type: req.body.type
             });
             order = await order.save();
+            if (!order) return res.status(400).send({ message: "Order could not be created" });
             res.status(200).send({ message: `Order with ID ${order._id} has gone through successfully.` });
         }
         catch (error) {
             res.status(500).send({ message: error.message });
         }
-    })
+    });
 
 router.route("/:id")
     .get(async (req, res) => {
@@ -87,60 +92,60 @@ router.route("/:id")
             res.status(500).send({ message: error.message });
         }
     })
-    .delete(async (req, res) => {
-        Order.findByIdAndRemove(req.params.id)
-            .then(async (order) => {
-                if (order) {
-                    await order.orderItems.map(async orderItem => {
-                        await OrderItem.findByIdAndRemove(orderItem)
-                    })
-                    return res.status(200).send({ message: `Order with ID ${OrderItem._id} has been deleted` });
-                }
-                else {
-                    return res.status(404).send({ message: "No such order found" });
-                }
-            })
-            .catch((error) => {
-                return res.status(500).send({ message: error.message });
-            })
+    .delete((req, res) => {
+        Order.findByIdAndDelete(req.params.id).then(async (order) => {
+            if (order) {
+                await order.orderItems.map(async orderItem => {
+                    await OrderItem.findByIdAndDelete(orderItem)
+                })
+                return res.status(200).json({ message: `Order with ID ${req.params.id} has been deleted` });
+            } else {
+                return res.status(404).json({ message: "Order not found" })
+            }
+        }).catch(err => {
+            return res.status(500).json({ success: false, error: err })
+        })
     })
 
+//TODO: Needs admin auth
 router.get("/get/totalsales", async (req, res) => {
     try {
         const totalSales = await Order.aggregate([
             { $group: { _id: null, totalsales: { $sum: "$totalPrice" } } }
         ]);
-        if (!totalSales) res.status(404).send({message: "No sales found"});
+        if (!totalSales) res.status(404).send({ message: "No sales found" });
         else res.status(200).send(totalSales);
     }
     catch (error) {
-        res.status(500).send({message: error.message});
+        res.status(500).send({ message: error.message });
     }
 });
 
+//TODO: Needs admin auth
 router.get("/get/count", async (req, res) => {
     try {
         const orderCount = await Order.countDocuments((count) => count);
-        if (!orderCount) res.status(404).send({message: "No orders found"});
+        if (!orderCount) res.status(404).send({ message: "No orders found" });
         else res.status(200).send(orderCount);
     }
     catch (error) {
-        res.status(500).send({message: error.message});
+        res.status(500).send({ message: error.message });
     }
 });
 
+//TODO: Needs admin auth
 router.get("/get/userorders/:userid", async (req, res) => {
     try {
-        const userOrderList = await Order.find({user: req.params.find}).populate({
+        const userOrderList = await Order.find({ user: req.params.find }).populate({
             path: "orderItems", populate: {
                 path: "product", populate: "category"
             }
-        }).sort({"dateOrdered": -1});
-        if (!userOrderList) res.status(404).send({message: "No orders found"});
+        }).sort({ "dateOrdered": -1 });
+        if (!userOrderList) res.status(404).send({ message: "No orders found" });
         else res.status(200).send(userOrderList);
     }
     catch (error) {
-        res.status(500).send({message: error.message});
+        res.status(500).send({ message: error.message });
     }
 });
 
