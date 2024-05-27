87bc97678942a5f933a47dee01154c51ce44a2e0
TarikEren <1210606015@nku.edu.tr>
2024-05-27 20:12:56 +0300

    Code cleanup

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -47,15 +47,15 @@ const orderSchema = mongoose.Schema({
     },
 })
 
-orderSchema.virtual('id').get(function () {
+orderSchema.virtual("id").get(function () {
     return this._id.toHexString();
 });
 
-orderSchema.set('toJSON', {
+orderSchema.set("toJSON", {
     virtuals: true,
 });
 
-exports.Order = mongoose.model('Order', orderSchema);
+exports.Order = mongoose.model("Order", orderSchema);
 
 
 
