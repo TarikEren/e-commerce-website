141c1527038c5fbaa0a8e0989275de5ce471a381
TarikEren <1210606015@nku.edu.tr>
2024-06-01 17:42:07 +0300

    Set the user route functions

---
+++ backend/routes/api/user.js
@@ -0,0 +1,37 @@
+const router = require("express").Router();
+const {
+    getAllUsers,
+    getUser,
+    updateUser,
+    deleteUser,
+    getCart,
+    addToCart,
+    removeFromCart,
+    clearCart,
+    getOldPayments,
+    addOldPayment,
+    removeOldPayment
+} = require("../../controllers/userController");
+
+router.route("/")
+    .get(getAllUsers)
+    .put(updateUser)
+    .delete(deleteUser);
+
+router.route("/:id")
+    .get(getUser);
+
+router.route("/:id/cart")
+    .get(getCart)
+    .post(addToCart)
+    .delete(removeFromCart);
+
+router.route("/:id/checkout")
+    .get(clearCart);
+
+router.route("/:id/payment-history")
+    .get(getOldPayments)
+    .post(addOldPayment)
+    .delete(removeOldPayment)
+
+module.exports = router;
