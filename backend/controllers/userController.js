84e1d79f91c73a47e31dd55e0a746ae2a3d378d0
TarikEren <1210606015@nku.edu.tr>
2024-06-01 18:19:39 +0300

    Added cart controls

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require("../models/userModel");
+const Product = require("../models/productModel");
 
 const getAllUsers = async (req, res) => {
     //Find all users
@@ -74,7 +75,7 @@ const deleteUser = async (req, res) => {
 }
 
 const getCart = async (req, res) => {
-    //If request body or ID section doesn't exist send 400
+    //If ID section doesn't exist send 400
     if (!req?.params?.id) return res.status(400).send({ "message": "User ID required" });
 
     //Find user by ID
@@ -85,18 +86,67 @@ const getCart = async (req, res) => {
 
     //Get the cart of the user and send it
     const cart = user.cart;
+    if (cart === null) return res.status(500).send({ "message": "User doesn't have the property 'Cart'" });
     res.send(cart);
 }
 
 const addToCart = async (req, res) => {
-    
+    //If ID section doesn't exist send 400
+    if (!req?.params?.id) return res.status(400).send({ "message": "User ID required" });
+
+    //Find user by ID
+    const user = await User.findById(req.params.id);
+
+    //If user is not found send 204
+    if (!user) return res.status(204).send({ "message": "No user found" });
+
+    //If product ID doesn't exist send 400
+    if (!req?.body?.product) return res.status(400).send({ "message": "Product ID required" });
+
+    //Find product by ID
+    const product = await Product.findById(req.body.product);
+
+    //If product is not found send 204
+    if (!product) return res.status(204).send({ "Message": "Product not found" });
+
+    //If product is found, push the product ID to the user cart
+    user.cart.push(product._id);
+
+    //Save the user and send the result
+    const result = await user.save();
+    res.send(result);
+
 }
 
 const removeFromCart = async (req, res) => {
+    //If ID section doesn't exist send 400
+    if (!req?.params?.id) return res.status(400).send({ "message": "User ID required" });
 
+    //Find user by ID
+    const user = await User.findById(req.params.id);
+
+    //If user is not found send 204
+    if (!user) return res.status(204).send({ "message": "No user found" });
+
+    //If product ID doesn't exist send 400
+    if (!req?.body?.product) return res.status(400).send({ "message": "Product ID required" });
+
+    //Check if the product is in the cart
+    const product = user.cart.findIndex((pID) => pID === req.body.product);
+
+    //If the product isn't in the cart send 204
+    if (!product) return res.status(204).send({"message": "Product is not in the cart"});
+
+    //Filter the cart so that the array does not contain the to be deleted product
+    const otherProducts = user.cart.filter((ID) => product !== ID);
+    user.cart = otherProducts;
+
+    //Save the user and send the result
+    const result = await user.save();
+    res.send(result);
 }
 
-const clearCart = async (req, res) => {
+const payment = async (req, res) => {
 
 }
 
@@ -121,7 +171,7 @@ module.exports = {
     getCart,
     addToCart,
     removeFromCart,
-    clearCart,
+    payment,
     getOldPayments,
     addOldPayment,
     removeOldPayment
