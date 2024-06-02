34f740a5a7b9dff85984eda64e5893d578fc130d
TarikEren <1210606015@nku.edu.tr>
2024-06-02 15:58:04 +0300

    Implemented getUser

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -12,47 +12,33 @@ const getAllUsers = async (req, res) => {
     res.send(users);
 }
 
-const getUser = async (req, res) => {
-    //If request body or ID section doesn't exist send 400
-    if (!req?.params?.id) return res.status(400).send({ "message": "User ID required" });
-
-    //Find user by ID
-    const user = await User.findById(req.params.id);
-
-    //If user is not found send 204
-    if (!user) return res.status(204).send({ "message": "No user found" });
-
+const sendUser = async (req, res) => {
+    //User retrieval is performed by the helper getUser function
+    const user = req.user;
     //Send user
     res.send(user);
 }
 
 const updateUser = async (req, res) => {
-    //If request body or ID section doesn't exist send 400
-    if (!req?.body?.id) return res.status(400).send({ "message": "User ID required" });
-
-    //Find user by ID
-    const user = await User.findById(req.body.id);
-
-    //If user is not found send 204
-    if (!user) return res.status(204).send({ "message": "No user found" });
-
+    //User retrieval is performed by the helper getUser function
+    const user = req.user
     //Check the body and if certain fields exist, edit them
     //This part may cause problems if it causes a double sendStatus issue
-    if (!req?.body?.email) {
+    if (req?.body?.email) {
         //Check if another user already has the new email.
         const emailExists = User.findOne({ email: req.body.email });
 
         //If the new email exists send 409 (Conflict)
-        if (userExists) return res.sendStatus(409);
+        if (emailExists) return res.sendStatus(409);
 
         //Change the current user's email
         user.email = req.body.email;
     }
     //If password is provided change the user's password
-    if (!req?.body?.password) user.password = req.body.password;
+    if (req?.body?.password) user.password = req.body.password;
 
     //If address is provided change the user's address
-    if (!req?.body?.address) user.address = req.body.address;
+    if (req?.body?.address) user.address = req.body.address;
 
     //Save the user and send the result
     const result = await user.save();
@@ -60,29 +46,15 @@ const updateUser = async (req, res) => {
 }
 
 const deleteUser = async (req, res) => {
-    //If request body or ID section doesn't exist send 400
-    if (!req?.body?.id) return res.status(400).send({ "message": "User ID required" });
-
-    //Find user by ID
-    const user = await User.findById(req.body.id);
-
-    //If user is not found send 204
-    if (!user) return res.status(204).send({ "message": "No user found" });
-
+    //User retrieval is performed by the helper getUser function   
     //Delete the user and send the result
-    const result = await User.findByIdAndDelete(req.body.id);
+    const result = await User.findByIdAndDelete(req.params.id);
     res.send(result);
 }
 
 const getCart = async (req, res) => {
-    //If ID section doesn't exist send 400
-    if (!req?.params?.id) return res.status(400).send({ "message": "User ID required" });
-
-    //Find user by ID
-    const user = await User.findById(req.params.id);
-
-    //If user is not found send 204
-    if (!user) return res.status(204).send({ "message": "No user found" });
+    //User retrieval is performed by the helper getUser function
+    const user = req.user;
 
     //Get the cart of the user and send it
     const cart = user.cart;
@@ -91,14 +63,8 @@ const getCart = async (req, res) => {
 }
 
 const addToCart = async (req, res) => {
-    //If ID section doesn't exist send 400
-    if (!req?.params?.id) return res.status(400).send({ "message": "User ID required" });
-
-    //Find user by ID
-    const user = await User.findById(req.params.id);
-
-    //If user is not found send 204
-    if (!user) return res.status(204).send({ "message": "No user found" });
+    //User retrieval is performed by the helper getUser function
+    const user = req.user;
 
     //If product ID doesn't exist send 400
     if (!req?.body?.product) return res.status(400).send({ "message": "Product ID required" });
@@ -119,14 +85,8 @@ const addToCart = async (req, res) => {
 }
 
 const removeFromCart = async (req, res) => {
-    //If ID section doesn't exist send 400
-    if (!req?.params?.id) return res.status(400).send({ "message": "User ID required" });
-
-    //Find user by ID
-    const user = await User.findById(req.params.id);
-
-    //If user is not found send 204
-    if (!user) return res.status(204).send({ "message": "No user found" });
+    //User retrieval is performed by the helper getUser function
+    const user = req.user;
 
     //If product ID doesn't exist send 400
     if (!req?.body?.product) return res.status(400).send({ "message": "Product ID required" });
@@ -135,7 +95,7 @@ const removeFromCart = async (req, res) => {
     const product = user.cart.findIndex((pID) => pID === req.body.product);
 
     //If the product isn't in the cart send 204
-    if (!product) return res.status(204).send({"message": "Product is not in the cart"});
+    if (!product) return res.status(204).send({ "message": "Product is not in the cart" });
 
     //Filter the cart so that the array does not contain the to be deleted product
     const otherProducts = user.cart.filter((ID) => product !== ID);
@@ -146,6 +106,7 @@ const removeFromCart = async (req, res) => {
     res.send(result);
 }
 
+//IMPLEMENT
 const payment = async (req, res) => {
 
 }
@@ -165,7 +126,7 @@ const removeOldPayment = async (req, res) => {
 
 module.exports = {
     getAllUsers,
-    getUser,
+    sendUser,
     updateUser,
     deleteUser,
     getCart,
