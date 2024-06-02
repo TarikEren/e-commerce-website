ff01ebee44f27579c127ef94cc4f4f06655d6b9e
TarikEren <1210606015@nku.edu.tr>
2024-06-02 19:19:47 +0300

    Added getUser middleware

--- backend/middleware/getUser.js
+++ backend/middleware/getUser.js
@@ -1,14 +1,12 @@
 const User = require("../models/userModel");
 
 const getOneUser = async (req, res, next) => {
-    //If request body or ID section doesn't exist send 400
-    if (!req?.params?.id) return res.status(400).send({ "message": "User ID required" });
-
+    if (!req?.body?.id) return res.status(400).send({"message": "ID required"})
     //Find user by ID
-    const user = await User.findById(req.params.id);
+    const user = await User.findById(req.body.id);
 
     //If user is not found send 204
-    if (!user) return res.status(204).send({ "message": "No user found" });
+    if (!user) return res.sendStatus(404);
 
     req.user = user;
     next();
